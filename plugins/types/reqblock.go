0c6d7fb86d0f96b5227e1bc0816a89a276797f2e
cancer-chain <cancer@kuchain.io>
2020-08-19 18:24:33 +0800

    modify  ReqBeginBlock struct

--- plugins/types/reqblock.go
+++ plugins/types/reqblock.go
@@ -7,14 +7,13 @@ import (
 )
 
 type ReqBeginBlock struct {
-	RequestBeginBlock types.Block
-	Tx                []ReqTx
-	Events            ReqEvents
-	TxEvents          []ReqEvents
-	FeeEvents         ReqEvents
-
-	ValidatorInfo string
-	Time          time.Time
+	RequestBeginBlock types.Block `json:"request_begin_block"`
+	Tx                []ReqTx     `json:"tx"`
+	Events            ReqEvents   `json:"events"`
+	TxEvents          []ReqEvents `json:"tx_events"`
+	FeeEvents         ReqEvents   `json:"fee_events"`
+	ValidatorInfo     string      `json:"validator_info"`
+	Time              time.Time   `json:"time"`
 }
 
 type ReqEndBlock struct {
